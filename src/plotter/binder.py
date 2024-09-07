4c88b70249790f1fd4790d69369bab6ed26892db
Kseniia Tuholukova <k.tuholukova@gmail.com>
2024-09-07 21:34:19 +0300

    Add Binder class

---
+++ src/plotter/binder.py
@@ -0,0 +1,28 @@
+from warnings import warn
+
+import pandas as pd
+from typeguard import typechecked
+
+
+class Binder:
+    @typechecked
+    def __init__(self, df: pd.DataFrame) -> None:
+        """Binds data for plotting"""
+        self.df = df
+
+    @typechecked
+    def bind(self, col_x: str, col_y: str, col_z: str) -> pd.DataFrame:
+        """Binds values in x and y columns based on z column"""
+        df1 = self.df[[col_z, col_x]].drop_duplicates()
+        df2 = self.df[[col_z, col_y]].drop_duplicates()
+
+        out_df = df1.merge(df2, on=col_z, how="outer").dropna()
+
+        if out_df[col_y].nunique() < self.df[col_y].nunique():
+            missing_values = list(set(self.df[col_y]) - set(out_df[col_y]))
+            warn(
+                f"Values {missing_values} cannot be bound to a certain {col_z} value "
+                f"via {col_z}. Try binning {col_z} or setting a lower number of bins."
+            )
+
+        return out_df
