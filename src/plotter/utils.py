77c0f653075168e4da7c0287927a3be2a072552a
Kseniia Tuholukova <k.tuholukova@gmail.com>
2024-09-07 22:18:06 +0300

    Add utils

---
+++ src/plotter/utils.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from typeguard import typechecked
+
+
+@typechecked
+def bin_data(data: pd.Series, n_bins: int) -> pd.DataFrame:
+    """Bins values in a series into a specified number of bins"""
+    assert (
+        1 <= n_bins <= len(data)
+    ), f"Invalid number of bins, should be between 1 and {len(data)}"
+
+    return pd.cut(data, bins=n_bins)
