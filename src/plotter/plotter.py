51196166795f65e97c2e28a86ee980efc79462ff
Kseniia Tuholukova <k.tuholukova@gmail.com>
2024-09-08 12:24:40 +0300

    Add fig.show to plotter

--- src/plotter/plotter.py
+++ src/plotter/plotter.py
@@ -12,11 +12,17 @@ class Plotter:
     @typechecked
     def plot_unbound(self, col_x: str, col_y: str, out_filepath: str) -> None:
         """Plots the data"""
-        fig = px.scatter(self.df, x=col_x, y=col_y)
+        fig = px.scatter(
+            self.df,
+            x=col_x,
+            y=col_y,
+        )
+        fig.show()
         fig.write_html(out_filepath)
 
     @typechecked
     def plot_bound(self, col_x: str, col_y: str, col_z: str, out_filepath: str) -> None:
         """Plots the data with binding"""
         fig = px.scatter(self.df, x=col_x, y=col_y, hover_data=[col_z])
+        fig.show()
         fig.write_html(out_filepath)
