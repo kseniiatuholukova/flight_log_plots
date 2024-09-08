432f9d0d92733dcefefbc3532f2f362d388963b3
Kseniia Tuholukova <k.tuholukova@gmail.com>
2024-09-08 18:21:43 +0300

    Update binder
    Rename arguments, change warning message, drop nans in missing values

--- src/plotter/binder.py
+++ src/plotter/binder.py
@@ -11,21 +11,24 @@ class Binder:
         self.df = df
 
     @typechecked
-    def bind(self, col_x: str, col_y: str, col_z: str) -> pd.DataFrame:
+    def bind(self, col_x: str, col_y: str, col_to_bind_by: str) -> pd.DataFrame:
         """Binds values in x and y columns based on z column"""
-        for col in [col_x, col_y, col_z]:
+        for col in [col_x, col_y, col_to_bind_by]:
             assert col in self.df.columns, f"Column {col} not found in the DataFrame"
 
-        df1 = self.df[[col_z, col_x]].drop_duplicates()
-        df2 = self.df[[col_z, col_y]].drop_duplicates()
+        df1 = self.df[[col_to_bind_by, col_x]].drop_duplicates()
+        df2 = self.df[[col_to_bind_by, col_y]].drop_duplicates()
 
-        out_df = df1.merge(df2, on=col_z, how="outer").dropna()
+        out_df = df1.merge(df2, on=col_to_bind_by, how="outer").dropna()
 
         if out_df[col_y].nunique() < self.df[col_y].nunique():
-            missing_values = list(set(self.df[col_y]) - set(out_df[col_y]))
+            missing_values = list(
+                set(self.df[col_y].dropna()) - set(out_df[col_y].dropna())
+            )
             warn(
-                f"Values {missing_values} cannot be bound to a certain {col_z} value "
-                f"via {col_z}. Try binning {col_z} or setting a lower number of bins."
+                f"{missing_values} {col_y} values cannot be bound to a certain {col_x} "
+                f"value via the third variable. Try binning the third variable or set "
+                "a lower number of bins."
             )
 
         return out_df
