af52fdf75638cd67f80337dd64dc66d2e7af269e
Kseniia Tuholukova <k.tuholukova@gmail.com>
2024-09-08 16:01:12 +0300

    Reduce plotter methods

--- src/plotter/plotter.py
+++ src/plotter/plotter.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pandas as pd
 import plotly.express as px
 from typeguard import typechecked
@@ -12,27 +14,40 @@ class Plotter:
         self.df = df
 
     @typechecked
-    def plot_unbound(self, col_x: str, col_y: str, out_filepath: str) -> None:
+    def plot(
+        self,
+        col_x: str,
+        col_y: str,
+        out_filepath: str,
+        hover_data_col: Optional[str] = None,
+        show_fig: bool = False,
+    ) -> None:
         """Plots the data"""
         assert out_filepath.endswith(FileFormat.HTML), (
             f"plots are saved in {FileFormat.HTML} format. Please provide a valid "
             "output filepath"
         )
 
-        for col in [col_x, col_y]:
-            assert col in self.df.columns, f"Column {col} not found in the DataFrame"
+        for col in [col_x, col_y, hover_data_col]:
+            if col is not None and col not in self.df.columns:
+                raise ValueError(f"Column {col} not found in the DataFrame")
 
-        fig = px.scatter(
-            self.df,
-            x=col_x,
-            y=col_y,
-        )
-        fig.show()
-        fig.write_html(out_filepath)
+        if hover_data_col is not None:
+            fig = px.scatter(
+                self.df,
+                x=col_x,
+                y=col_y,
+                hover_data=[hover_data_col],
+            )
 
-    @typechecked
-    def plot_bound(self, col_x: str, col_y: str, col_z: str, out_filepath: str) -> None:
-        """Plots the data with binding"""
-        fig = px.scatter(self.df, x=col_x, y=col_y, hover_data=[col_z])
-        fig.show()
+        else:
+            fig = px.scatter(
+                self.df,
+                x=col_x,
+                y=col_y,
+            )
+
+        if show_fig:
+            fig.show()
+
         fig.write_html(out_filepath)
