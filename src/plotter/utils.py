3d85d2e2f717041b30a611c51c0643dfabcb5715
Kseniia Tuholukova <k.tuholukova@gmail.com>
2024-09-08 11:54:33 +0300

    Convert bin categories to string for plotting

--- src/plotter/utils.py
+++ src/plotter/utils.py
@@ -3,10 +3,10 @@ from typeguard import typechecked
 
 
 @typechecked
-def bin_data(data: pd.Series, n_bins: int) -> pd.DataFrame:
+def bin_data(data: pd.Series, n_bins: int) -> pd.Series:
     """Bins values in a series into a specified number of bins"""
     assert (
         1 <= n_bins <= len(data)
     ), f"Invalid number of bins, should be between 1 and {len(data)}"
 
-    return pd.cut(data, bins=n_bins)
+    return pd.cut(data, bins=n_bins).astype(str)
